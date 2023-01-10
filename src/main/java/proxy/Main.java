49d90bd7b9d9c6ab69df60697da0285bba2d88be
Shchur Oleksandr <Olexander.Shchur@gmail.com>
2023-01-10 19:25:18 +0200

    Some fixes

--- src/main/java/proxy/Main.java
+++ src/main/java/proxy/Main.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
         Scrapper scrapper = new ScrapperProxy(new URLScrapper());
-        System.out.println(scrapper.parseHtmlByUrl("https://www.5.ua/", ""));
+        System.out.println(scrapper.parseHtmlByUrl("https://time.is/", ""));
     }
 }
--- src/test/java/proxy/ScrapperProxyTest.java
+++ src/test/java/proxy/ScrapperProxyTest.java
@@ -4,12 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
 
 public class ScrapperProxyTest {
     @Test
     public void testProxy() throws SQLException, IOException, InterruptedException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:HtmlCache");
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("DROP TABLE Cache");
+        stmt.executeUpdate("create table Cache" +
+                "(" +
+                "    url    text," +
+                "    html text" +
+                ");");
+        stmt.close();
+
         Scrapper scrapper = new ScrapperProxy(new URLScrapper());
         String firstResult = scrapper.parseHtmlByUrl("https://time.is/", "");
         TimeUnit.SECONDS.sleep(2);
